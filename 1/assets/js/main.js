c4a997269623a2d178e74b91220beef346330ef4
hasekoikuta <30440577+hasekoikuta@users.noreply.github.com>
2020-11-02 11:14:59 +0900

    a

--- 1/assets/js/main.js
+++ 1/assets/js/main.js
@@ -1,26 +1,12 @@
         function NavOpen() {
-            target = document.getElementById("nav-close");
-            target.id = "nav-open";
-            target = document.getElementById("nav-shadow");
-            target.id = "nav-shadow-active";
-            target = document.getElementById("form-fixed");
-            target.id = "form-none";
-            target = document.getElementById("form-fixed2");
-            target.id = "form-none2";
+			target = document.getElementById("schedule-close");
+			target2 = document.getElementById("schedule-open");
+            target.id = "schedule-open";
+			target2.id = "schedule-close";
             return false;
         }
         function NavClose() {
-            target = document.getElementById("nav-open");
-            target.id = "nav-close";
-            target = document.getElementById("nav-shadow-active");
-            target.id = "nav-shadow";
-		    target = document.getElementById("form-none");
-            target.id = "form-fixed";	
-			target = document.getElementById("form-none2");
-            target.id = "form-fixed2";
+			target = document.getElementById("schedule-open");
+            target.id = "schedule-close";
             return false;
         }
-		window.onload = function() {
-  const spinner = document.getElementById('pattern');
-  spinner.classList.add('loaded');
-}
